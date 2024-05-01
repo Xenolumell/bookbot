76ea15e192614a429c2955a4735011b6068d92b2
Xenolumell <JLume21@gmail.com>
2024-05-01 18:42:18 -0400

    update: finished the dictionary for conting letters

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-def main():
-    with open("books/frankenstein.txt") as f:
+with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+
+def word_count():
     words = file_contents.split()
     word_number = 0
     for word in words:
         word_number += 1
-    print(word_number)
+    return word_number
+
+def letter_count():
+    words = file_contents.split()
+    letters_dict = {}
+    for word in words:
+        letters = word.split()
+        for letter in letters:
+            letter.split()
+            for l in letter:
+                i = l.lower()
+                if i in letters_dict:
+                    letters_dict[i] += 1
+                else:
+                    letters_dict[i] = 1
+    return letters_dict
+
+
+def main():
+    print(word_count())
+    print(letter_count())
 
 main()
